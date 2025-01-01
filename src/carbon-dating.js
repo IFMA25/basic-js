edc0ef12b4d8fd500b7026ac8b1337a8ee06877a
Inna Fedorova <innamassura@gmail.com>
2025-01-01 23:02:31 +0200

    add: task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
+  if(!isFinite(parseFloat(sampleActivity)) || typeof(sampleActivity) !== 'string' || parseFloat(sampleActivity) <=0 || parseFloat(sampleActivity) > MODERN_ACTIVITY){
+    return false
+  }
+  const result = Math.ceil(Math.log(MODERN_ACTIVITY/parseFloat(sampleActivity)) / (0.693/HALF_LIFE_PERIOD))
+  return result
 }
 
 module.exports = {
