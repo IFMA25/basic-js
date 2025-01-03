16b1f341713c05e71bcd1a60c766b9911424f69c
Inna Fedorova <innamassura@gmail.com>
2025-01-03 20:16:32 +0200

    add:task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+
+  domains.forEach(item => {
+    let itemArr = item.split('.');
+    let subDomain = ''
+    for(let i = itemArr.length-1; i >= 0; i-- ){
+      subDomain += '.'+itemArr[i];
+      if(result[subDomain]){
+        result[subDomain] += 1
+      } else{
+        result[subDomain] = 1
+      }
+    }
+  });
+  
+  return result
 }
 
 module.exports = {
